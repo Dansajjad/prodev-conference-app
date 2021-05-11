f6d4cb661ce92bc189edc85e4ed929adbbf3a3f9
Dan Sajjad <dan.sajjad@gmail.com>
2021-05-11 15:42:49 -0400

    Update routes in badges and in backend to include only relevant routes

--- badges/src/routes/index.mjs
+++ badges/src/routes/index.mjs
@@ -1,10 +1,6 @@
 import Router from '@koa/router';
-import { router as swaggerRouter } from './swagger.mjs';
-import { router as attendeesRouter } from './attendees.mjs';
 import { router as badgesRouter } from './badges.mjs';
 
 export const router = new Router();
 
-router.use(swaggerRouter.routes()).use(swaggerRouter.allowedMethods());
-router.use('/api', attendeesRouter.routes(), attendeesRouter.allowedMethods());
 router.use('/api', badgesRouter.routes(), badgesRouter.allowedMethods());
